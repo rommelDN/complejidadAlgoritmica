c399dd3365acbc752f6dd29e44e22f2c27bbeed6
Mathias Acuache <u202314898@upc.edu.pe>
2026-05-10 16:54:27 -0500

    scc subgrafo

--- scc.py
+++ scc.py
@@ -1,18 +1,19 @@
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
+import math
 
-# Aumentar límite para grafos de 1500 nodos
-sys.setrecursionlimit(3000)
+sys.setrecursionlimit(5000)
 
 df = pd.read_csv("grafo_biologico_1500.csv")
-# Construcción manual de lista de adyacencia
+
 adj = {}
 for u, v, _ in df.values:
     if u not in adj: adj[u] = []
     adj[u].append(v)
     if v not in adj: adj[v] = []
 
-def scc_tarjan_manual(grafo):
+def algoritmo_scc(grafo):
     indice = 0
     stack = []
     on_stack = {u: False for u in grafo}
@@ -48,7 +49,30 @@ def scc_tarjan_manual(grafo):
             fuerte_conexion(n)
     return resultado
 
-# Ejecución y reporte
-sccs = scc_tarjan_manual(adj)
-print(f"Total de componentes (SCC): {len(sccs)}")
-print(f"Ciclos detectados: {len([s for s in sccs if len(s) > 1])}")
+sccs_detectados = algoritmo_scc(adj)
+print(f"Total de componentes (SCC): {len(sccs_detectados)}")
+ciclos = [s for s in sccs_detectados if len(s) > 1]
+print(f"Ciclos biológicos encontrados: {len(ciclos)}")
+
+if ciclos:
+    scc_ver = max(ciclos, key=len)[:20]
+    n_set = set(scc_ver)
+    pos = {}
+    r = 10
+    for i, nodo in enumerate(scc_ver):
+        a = 2 * math.pi * i / len(scc_ver)
+        pos[nodo] = (r * math.cos(a), r * math.sin(a))
+
+    plt.figure(figsize=(10, 10))
+    df_plot = df[df['protein1'].isin(n_set) & df['protein2'].isin(n_set)]
+
+    for u, v in zip(df_plot['protein1'], df_plot['protein2']):
+        plt.plot([pos[u][0], pos[v][0]], [pos[u][1], pos[v][1]], color='gray', alpha=0.5)
+
+    for n, (x, y) in pos.items():
+        plt.scatter(x, y, s=700, color='#5DADE2', edgecolors='black')
+        plt.text(x, y, n, fontsize=8, ha='center', va='center', fontweight='bold')
+
+    plt.title("Visualización del Algoritmo SCC ")
+    plt.axis('off')
+    plt.show()
